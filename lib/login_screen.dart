220f75c9803965666f6c5af3a83826752b6b7d52
Chandra Perdiansyah <66311673+cperdiansyah@users.noreply.github.com>
2022-06-15 12:24:52 +0700

    add responsive to login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -7,6 +7,27 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return LoginResponsive();
+        },
+      ),
+    );
+  }
+}
+
+class LoginResponsive extends StatefulWidget {
+  @override
+  _LoginResponsive createState() => _LoginResponsive();
+}
+
+class _LoginResponsive extends State<LoginResponsive> {
+  bool _isObscure = true;
+
+  // const LoginResponsive({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -24,43 +45,61 @@ class LoginScreen extends StatelessWidget {
             ),
             Column(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Column(
-                    children: <Widget>[
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Email',
-                          prefixIcon: Icon(Icons.email),
-                        ),
-                      ),
-                      TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Password',
-                          prefixIcon: Icon(Icons.lock),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 16.0),
-                        child: ElevatedButton(
-                          child: Text(
-                            'Login',
-                            style: TextStyle(fontSize: 16),
-                          ),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => HomeScreen(),
-                              ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: const Size.fromHeight(50),
+                Container(
+                  width: MediaQuery.of(context).size.width <= 600
+                      ? MediaQuery.of(context).size.width
+                      : MediaQuery.of(context).size.width * 0.3,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            prefixIcon: Icon(Icons.email),
                           ),
                         ),
-                      ),
-                    ],
+                        TextField(
+                          obscureText: _isObscure,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: IconButton(
+                              icon: Icon(_isObscure
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    _isObscure = !_isObscure;
+                                  },
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 16.0),
+                          child: ElevatedButton(
+                            child: Text(
+                              'Login',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => HomeScreen(),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size.fromHeight(50),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
